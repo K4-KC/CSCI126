fa95ac0bbaf5d00e221a4a428aa29de171020773
K4_keyChan_ <72117307+K4-KC@users.noreply.github.com>
2022-10-14 15:15:46 -0400

    Create mod15.m

---
+++ mod15.m
@@ -0,0 +1,37 @@
+%Script to plot range against angle (projectile launched at ground level,
+% toward a cliff)
+
+clear;
+clc;
+
+g = 9.8;
+
+disp('Plot of range (m) versus angle (0 to 90 degrees, 1/2-degree increments)');
+h = input('Enter depth of chasm: ');
+dist = input('Enter width of chasm: ');
+v = input('Enter the initial velocity (m/s): ');
+
+for i = 1:181
+    angles(i) = (i-1)/2;
+    v0x = v*cosd(angles(i)) + 1e-6;
+    v0y = v*sind(angles(i));
+    t_to_cliff = dist/v0x;
+    y = v0y*t_to_cliff - (g*t_to_cliff^2)/2;
+    if(y < -h)
+        ranges(i) = range2(v, angles(i), -h);
+    elseif(y < 0)
+        ranges(i) = dist;
+    else
+        ranges(i) = range2(v, angles(i), 0);
+    end
+end
+
+plot(angles,ranges);
+grid on;
+title('Projectile Fired Towards a Cliff');
+xlabel('Angle of Projectile (degrees)');
+ylabel('Range (m)');
+
+[max_r, max_i] = max(ranges);
+disp('  Max Range  Angle');
+disp([max_r, angles(max_i)]);
