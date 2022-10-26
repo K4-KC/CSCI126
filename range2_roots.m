d752bd65ee3917f9409563b3446d257b77cc20c1
K4_keyChan_ <72117307+K4-KC@users.noreply.github.com>
2022-10-26 17:51:57 -0400

    Create range2_roots.m

---
+++ range2_roots.m
@@ -0,0 +1,20 @@
+function r = range2(v, angle, s)
+%This function calculates the range of a projectile motion
+%Inputs are Velocity(m/s) and Angle(degrees) and height of cliff(meters)
+%range(v, angle, height)
+y = s;
+g = 9.8;
+
+v0y = v * sind(angle);
+v0x = v * cosd(angle);
+
+a = g/2;
+b = -v0y;
+c = y;
+
+r = roots([a b c]);
+
+t = max(r);
+
+r = t * v0x;
+end
