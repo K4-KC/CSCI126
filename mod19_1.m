fdbec9bc21efc3eb9a6785dea68024fb0c343825
K4_keyChan_ <72117307+K4-KC@users.noreply.github.com>
2022-11-04 15:05:37 -0400

    Update mod19_1.m

--- mod19_1.m
+++ mod19_1.m
@@ -6,17 +6,17 @@ C = cos(angles);
 S = sin(angles);
 
 subplot(3,1,1);
-plot(angles,C^2);
+plot(angles,C.^2);
 grid on;
 title("cos^2");
 
 subplot(3,1,2);
-plot(angles,S^2);
+plot(angles,S.^2);
 grid on;
 title("sin^2");
 
 subplot(3,1,3);
-plot(angles,C^2+S^2);
+plot(angles,C.^2+S.^2);
 grid on;
 title("sin^2 + cos^2");
-x_label("radians");
+xlabel("radians");
